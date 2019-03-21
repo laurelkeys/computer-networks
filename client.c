f266ca27841ef29256f687af3b9fdee5ac075770
Tiago Loureiro Chaves <ra187690@mamonas.lab.ic.unicamp.br>
2019-03-21 10:10:57 -0300

    Update client-server with example from Beej

--- client.c
+++ client.c
@@ -1,33 +1,93 @@
-#include "unp.h"
+/*
+** client.c -- a stream socket client demo
+*/
 
-int main(int argc, char **argv) {
-    int sock_fd;
-    struct sockaddr_in servaddr;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
-    if (argc != 2) err_quit("usage: tcpcli <IPaddress>");
+#include <arpa/inet.h>
 
-    sock_fd = Socket(PF_INET, SOCK_STREAM, 0);
+#define PORT "3490" // the port client will be connecting to
 
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(SERV_PORT);
+#define MAXDATASIZE 100 // max number of bytes we can get at once
 
-    Inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
 
-    Connect(sock_fd, (SA *) &servaddr, sizeof(servaddr));
-
-    str_cli(stdin, sock_fd); /* do it all */
-
-    exit(0);
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-str_cli(FILE *fp, int sock_fd) {
-    char sendline[MAXLINE], recvline[MAXLINE];
+int main(int argc, char *argv[])
+{
+	int sockfd, numbytes;
+	char buf[MAXDATASIZE];
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	char s[INET6_ADDRSTRLEN];
 
-    while (Fgets(sendline, MAXLINE, fp) != NULL) {
-        Writen(sock_fd, sendline, strlen (sendline));
-        if (Readline(sock_fd, recvline, MAXLINE) == 0)
-            rr_quit("str_cli: server terminated prematurely");
-        Fputs(recvline, stdout);
-    }
+	if (argc != 2) {
+	    fprintf(stderr,"usage: client hostname\n");
+	    exit(1);
+	}
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if ((rv = getaddrinfo(argv[1], PORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and connect to the first we can
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("client: socket");
+			continue;
+		}
+
+		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			perror("client: connect");
+			close(sockfd);
+			continue;
+		}
+
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "client: failed to connect\n");
+		return 2;
+	}
+
+	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+			s, sizeof s);
+	printf("client: connecting to %s\n", s);
+
+	freeaddrinfo(servinfo); // all done with this structure
+
+	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+	    perror("recv");
+	    exit(1);
+	}
+
+	buf[numbytes] = '\0';
+
+	printf("client: received '%s'\n",buf);
+
+	close(sockfd);
+
+	return 0;
 }
