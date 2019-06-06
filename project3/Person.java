f0b43b7ede2c7fdd589aed6628caf536f5f6f261
Carlos Avelar <carlos.amds@gmail.com>
2019-06-06 11:27:23 -0300

    Add Person.java with needed variables

---
+++ project3/Person.java
@@ -0,0 +1,12 @@
+
+public class Person {
+    private String email;
+    private String nome;
+    private String surname;
+    private String city;
+    private String education;
+    private Array<String> skills;
+    private Array<String> experiences;
+
+    // TODO getters and setters, or not?
+}
